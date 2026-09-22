d56496cafe2ecf8e1acbee9d215578a12c51c1c9
Poul Henriksen <pohe@zealand.dk>
2026-09-22 10:39:19 +0200

    er

--- Datastructures2026/Program.cs
+++ Datastructures2026/Program.cs
@@ -7,7 +7,7 @@ int i1 = 102;
 int i2 = 21;
 int i3 = 45;
 
-int[] myArray = new int[3];
+int[] myArray = new int[3]; //Fixed size
 
 myArray[0] = 102;
 myArray[1] = 21;
@@ -18,3 +18,21 @@ myArray[1] = 29;
 myArray[2] = myArray[1] + 10;
 
 Console.WriteLine(myArray[0] + " " + myArray[1] + " " + myArray[2]);
+int sum = myArray[0] + myArray[1] + myArray[2];
+Console.WriteLine("Summen er " + sum);
+
+
+for(int i = 0; i<3; i++)
+{
+    Console.Write(myArray[i] + " " );
+}
+Console.WriteLine();
+
+Console.WriteLine("Beregn summen af alle index positioner i myArray");
+
+int result=0; 
+for(int i =0; i < 3; i++)
+{
+    result = myArray[i] + result;
+}
+Console.WriteLine(" Summen er " + result);
