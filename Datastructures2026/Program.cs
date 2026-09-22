a3d7cccd64c610cea8a833f1a893b9255067c032
Poul Henriksen <pohe@zealand.dk>
2026-09-22 10:19:43 +0200

    ok

--- Datastructures2026/Program.cs
+++ Datastructures2026/Program.cs
@@ -1,1 +1,14 @@
 ﻿Console.WriteLine("Hello, datastructures!");
+
+string n1 = "Oliver";
+string n2 = "Carl";
+
+int i1 = 102;
+int i2 = 21;
+int i3 = 45;
+
+int[] myArray = new int[3];
+
+myArray[0] = 102;
+myArray[1] = 21;
+myArray[2] = 45; 
