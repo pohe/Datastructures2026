15fd94d5fa371bc0e1527c03e02afef1cda63318
Poul Henriksen <pohe@zealand.dk>
2026-09-22 10:23:27 +0200

    sd

--- Datastructures2026/Program.cs
+++ Datastructures2026/Program.cs
@@ -11,4 +11,10 @@ int[] myArray = new int[3];
 
 myArray[0] = 102;
 myArray[1] = 21;
-myArray[2] = 45; 
+myArray[2] = 45;
+
+myArray[1] = 29;
+
+myArray[2] = myArray[1] + 10;
+
+Console.WriteLine(myArray[0] + " " + myArray[1] + " " + myArray[2]);
