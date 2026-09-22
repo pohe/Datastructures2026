af2ee91fb06e59b518f7e1857a3035554766bfdf
Poul Henriksen <pohe@zealand.dk>
2026-09-22 11:31:13 +0200

    fd

--- Datastructures2026/Program.cs
+++ Datastructures2026/Program.cs
@@ -1,73 +1,89 @@
 ﻿Console.WriteLine("Hello, datastructures!");
 
-string n1 = "Oliver";
-string n2 = "Carl";
+//string n1 = "Oliver";
+//string n2 = "Carl";
 
-int i1 = 102;
-int i2 = 21;
-int i3 = 45;
+//int i1 = 102;
+//int i2 = 21;
+//int i3 = 45;
 
-int[] myArray = new int[3]; //Fixed size
+//int[] myArray = new int[3]; //Fixed size
 
-myArray[0] = 102;
-myArray[1] = 21;
-myArray[2] = 45;
+//myArray[0] = 102;
+//myArray[1] = 21;
+//myArray[2] = 45;
 
-myArray[1] = 29;
+//myArray[1] = 29;
 
-myArray[2] = myArray[1] + 10;
+//myArray[2] = myArray[1] + 10;
 
-Console.WriteLine(myArray[0] + " " + myArray[1] + " " + myArray[2]);
-int sum = myArray[0] + myArray[1] + myArray[2];
-Console.WriteLine("Summen er " + sum);
+//Console.WriteLine(myArray[0] + " " + myArray[1] + " " + myArray[2]);
+//int sum = myArray[0] + myArray[1] + myArray[2];
+//Console.WriteLine("Summen er " + sum);
 
 
-for(int i = 0; i<3; i++)
+//for(int i = 0; i<3; i++)
+//{
+//    Console.Write(myArray[i] + " " );
+//}
+//Console.WriteLine();
+
+//Console.WriteLine("Beregn summen af alle index positioner i myArray");
+
+//int result=0; 
+////for(int i =0; i < myArray.Length; i++)
+////{
+////    result = myArray[i] + result;
+////}
+//foreach( int num in myArray  )
+//{
+//    result = num + result; 
+//}
+//Console.WriteLine(" Summen er " + result);
+
+////Console.WriteLine(myArray[3]);
+
+
+//string[] names = new string[3];
+//names[0] = "Oliver";
+//names[1] = "Carl";
+//names[2] = "Wiktor";
+
+//foreach( string name in names)
+//{
+//    Console.WriteLine(name);
+//}
+
+//string searchName = "Ixping";
+//bool testResult = false; 
+//for(int i = 0; i<names.Length; i++)
+//{
+//    if (names[i] == searchName )
+//    {
+//        //Console.WriteLine($" {searchName} findes");
+//        testResult = true; 
+//        break;
+//    }
+//}
+//if ( testResult )
+//{
+//    Console.WriteLine($" {searchName} findes");
+//}
+//else
+//    Console.WriteLine($" {searchName} findes ikke");
+
+
+List<int> myFirstList = new List<int>();
+
+myFirstList.Add(10);
+myFirstList.Add(15); //Tilføjer bagerst i listen
+myFirstList.Add(27);
+myFirstList.Insert(2, 45);
+myFirstList.Remove(15);
+myFirstList.RemoveAt(1);
+
+foreach(int item in myFirstList)
 {
-    Console.Write(myArray[i] + " " );
+    Console.Write(item + " ");
 }
 Console.WriteLine();
-
-Console.WriteLine("Beregn summen af alle index positioner i myArray");
-
-int result=0; 
-//for(int i =0; i < myArray.Length; i++)
-//{
-//    result = myArray[i] + result;
-//}
-foreach( int num in myArray  )
-{
-    result = num + result; 
-}
-Console.WriteLine(" Summen er " + result);
-
-//Console.WriteLine(myArray[3]);
-
-
-string[] names = new string[3];
-names[0] = "Oliver";
-names[1] = "Carl";
-names[2] = "Wiktor";
-
-foreach( string name in names)
-{
-    Console.WriteLine(name);
-}
-
-string searchName = "Ixping";
-bool testResult = false; 
-for(int i = 0; i<names.Length; i++)
-{
-    if (names[i] == searchName )
-    {
-        //Console.WriteLine($" {searchName} findes");
-        testResult = true; 
-        break;
-    }
-}
-if ( testResult )
-{
-    Console.WriteLine($" {searchName} findes");
-}
-else
-    Console.WriteLine($" {searchName} findes ikke");
